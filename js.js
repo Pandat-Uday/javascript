0c87ce7d2e125219b30aa2ad02d3b8307c08cdaf
uday pandat <udaypandat2001@gmail.com>
2022-10-03 11:46:21 +0530

    create a form with use of javascript

---
+++ js.js
@@ -0,0 +1,22 @@
+var firstName = "Uday";
+var lastName = "Pandat";
+var middle_Name = "jayeshbhai";
+
+const price = 199;
+
+var courseName = "java script";
+
+var cardNumber = "6351-0207-1688-6625";
+
+var isPaid = true;
+var modeOfPayment;
+
+modeOfPayment = prompt("mode of Payment ?");
+
+alert(
+  firstName +
+    " has sucessfully bought " +
+    courseName +
+    "  Using  " +
+    modeOfPayment
+);
